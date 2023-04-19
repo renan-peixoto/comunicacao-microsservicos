5f5b29e0dbcfac6cfda3cb9c863aa02f9718e329
Renan Peixoto <renan2.peixoto@gmail.com>
2023-04-19 16:54:55 -0300

    sales-api config

---
+++ sales-api/app.js
@@ -0,0 +1,31 @@
+import express  from "express";
+
+import {connectMongoDb} from './src/configs/db/mongoDbConfig.js';
+import {createInitialData} from './src/configs/db/initialData.js';
+import checkToken from './src/configs/auth/checkToken.js';
+import {connectRabbitMq} from './src/configs/rabbitmq/rabbitConfig.js';
+
+
+
+const app = express();
+
+const env = process.env;
+const PORT = env.PORT || 8082;
+
+connectMongoDb();
+createInitialData();
+connectRabbitMq()
+
+app.use(checkToken);
+
+app.get('/api/status', async (req, res) => {
+    return res.status(200).json({
+        service: "Sales-API",
+        status: 'up',
+        httpStatus: 200
+    })
+})
+
+app.listen(PORT, () => {
+    console.info(`Server started sucessfully at port ${PORT}`);
+})
